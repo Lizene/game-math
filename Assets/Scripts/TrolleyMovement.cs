c08a32360b16c4e8435c480dce00ff696e1565ab
Oliver Vesa <olpertti@gmail.com>
2024-10-02 07:30:11 +0300

    Removed prints

--- Assets/Scripts/TrolleyMovement.cs
+++ Assets/Scripts/TrolleyMovement.cs
@@ -20,11 +20,8 @@ public class TrolleyMovement : MonoBehaviour
 
         cranePosition = position;
         craneUpperCenter = new Vector3(cranePosition.x, posY, cranePosition.z);
-        Debug.Log(craneUpperCenter);
         nearRadius = Vector3.Distance(craneUpperCenter,nearLimitObject.transform.position);
-        Debug.Log(nearRadius);
         farRadius = Vector3.Distance(craneUpperCenter,farLimitObject.transform.position);
-        Debug.Log(farRadius);
     }
     public void UpdateTransform(Vector3 craneForward)
     {
